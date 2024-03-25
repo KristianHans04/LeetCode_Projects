1c2a55eb27277f36495f4c08be6d904258a76bbf
KristianHans04 <theatwoodvlogers@gmail.com>
2024-03-25 23:51:00 +0300

    Second ACTUALLY WORKING solution

---
+++ Reverse_Words_in_a_String_III/Reverse_Words_in_a_String_III_Solution_2.py
@@ -0,0 +1,8 @@
+s = "Habari yako Mr Kristian"
+break_down = s.split()          # Splits the string into words
+broken_list = []
+for i in range(len(break_down)):
+        invert = break_down[i][::-1]#Prints the inverted string
+        broken_list.append(invert)
+
+b=" ".join(broken_list)
